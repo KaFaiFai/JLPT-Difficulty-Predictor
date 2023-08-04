489fe8b829ce86d6daeffbd80640821c8d04bcdf
Fai <m19990328@gmail.com>
2023-08-04 22:28:47 +0800

    add attention mask

--- models/attention.py
+++ models/attention.py
@@ -10,11 +10,11 @@ class SimpleAttention(nn.Module):
         super().__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_size)
         self.lstm = nn.LSTM(embedding_size, hidden_size, bidirectional=True, batch_first=True)
-        self.attention = nn.MultiheadAttention(hidden_size * 2, num_heads=1)
+        self.attention = nn.MultiheadAttention(hidden_size * 2, num_heads=1, batch_first=True)
         self.dropout = nn.Dropout(p=0.5)
         self.fc = nn.Linear(hidden_size * 2, num_class)
 
-    def forward(self, input_ids, **kwargs):
+    def forward(self, input_ids, attention_mask, **kwargs):
         # (B, seq_len) -> (B, num_calss)
 
         # Embedding layer
@@ -29,19 +29,27 @@ class SimpleAttention(nn.Module):
         # Attention layer: self-attention
         # (B, seq_len, hidden_size * 2) -> (B, hidden_size * 2)
         attention_output, _ = self.attention(
-            lstm_output.transpose(0, 1),
-            lstm_output.transpose(0, 1),
-            lstm_output.transpose(0, 1),
+            lstm_output, lstm_output, lstm_output, key_padding_mask=attention_mask == 0
         )
-        attention_output = attention_output[0]
 
         # Dropout layer
-        dropout_output = self.dropout(attention_output)
+        dropout_output = self.dropout(attention_output[:, 0, ...])
 
         # Fully connected layer
         # (B, hidden_size * 2) -> (B, num_calss)
         fc_output = self.fc(dropout_output)
         return fc_output
+
+    def get_attention_output(self, input_ids, attention_mask, **kwargs):
+        # (B, seq_len), (B, seq_len) -> (seq_len, B, hidden_size*2), (B, hidden_size*2, hidden_size*2)
+
+        embedded = self.embedding(input_ids)
+        lstm_output, _ = self.lstm(embedded)
+        mask = attention_mask == 0
+        attention_output, attention_output_weights = self.attention(
+            lstm_output, lstm_output, lstm_output, key_padding_mask=mask
+        )
+        return attention_output, attention_output_weights
 
 
 def test():
@@ -49,20 +57,17 @@ def test():
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     tokenizer = AutoTokenizer.from_pretrained("cl-tohoku/bert-base-japanese")
-    net = SimpleAttention(5, len(tokenizer), 300, 500).to(device)
+    model = SimpleAttention(5, len(tokenizer), 300, 500).to(device)
 
     line = "吾輩は猫である。"
     line2 = "国家公務員"
     inputs = tokenizer([line, line2], return_tensors="pt", padding="max_length").to(device)
     print(f"{len(tokenizer)=}")
 
-    summary(net, input_data=inputs["input_ids"])
+    summary(model, **inputs)
 
-    out = net(inputs["input_ids"])
-    # network_state = net.state_dict()
-    # print("PyTorch model's state_dict:")
-    # for layer, tensor in network_state.items():
-    #     print(f"{layer:<45}: {tensor.size()}")
+    out = model(**inputs)
+    print(model)
 
 
 if __name__ == "__main__":
